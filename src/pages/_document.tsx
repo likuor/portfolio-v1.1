3339d63ab778a5e519e0d04770c0d44e3012d5b8
Koki Sakai <likuor0528@gmail.com>
2023-10-02 13:16:34 -0700

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html lang='en'>
+    <Head />
+    <body>
+      <Main />
+      <a href='http://google'>google</a>
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
