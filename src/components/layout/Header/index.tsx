64065cfcd8468425e7c6bf32e7c5f97318a84cc9
likuor <likuor0528@gmail.com>
2023-10-02 22:30:26 -0700

    add: husky test

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -6,7 +6,6 @@ import SocialMedias from './SocialMedias';
 const Header = () => (
   <header className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
     <div className='container mx-auto'>
-
       <div className='flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8'>
         {/* logo */}
         <Link href='/'>
