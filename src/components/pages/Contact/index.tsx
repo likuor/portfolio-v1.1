463066e83286c885a6840ce175ffcf5aa10748f0
likuor <likuor0528@gmail.com>
2023-12-18 14:56:03 -0800

    add: adjust contact page

--- src/components/pages/Contact/index.tsx
+++ src/components/pages/Contact/index.tsx
@@ -27,13 +27,13 @@ const Contact = () => {
   return (
     <div className='h-full bg-primary/30'>
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
-        <div className='flex flex-col w-full max-w-[700px] f'>
+        <div className='flex flex-col w-full max-w-[700px]'>
           <motion.h2
             variants={fadeIn('up', 0.2)}
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='h2 text-center mb-12'
+            className='h2 text-center mb-4'
           >
             Get in Touch<span className='text-accent'>.</span>
           </motion.h2>
@@ -47,7 +47,7 @@ const Contact = () => {
             className='flex-1 flex flex-col gap-6 w-full mx-auto'
           >
             {/* input group */}
-            <form action='' onSubmit={handleSubmit(onSubmit)} className='flex-col w-full space-y-6'>
+            <form action='' onSubmit={handleSubmit(onSubmit)} className='flex-col w-full space-y-4'>
               <input
                 id='name'
                 type='text'
@@ -79,15 +79,17 @@ const Contact = () => {
                 {...register('message')}
               />
               <span>{errors.message?.message as ReactNode}</span>
-              <button
-                type='submit'
-                className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'
-              >
-                <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
-                  Let&apos;s chat
-                </span>
-                <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
-              </button>
+              <div className='flex justify-center'>
+                <button
+                  type='submit'
+                  className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'
+                >
+                  <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
+                    Let&apos;s chat
+                  </span>
+                  <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
+                </button>
+              </div>
             </form>
           </motion.div>
         </div>
