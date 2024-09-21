10572d5b794af8105c39bb3f3c4cec6e5c859674
likuor <likuor0528@gmail.com>
2024-09-21 21:20:19 +0900

    refactor: constants run fix

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -2,8 +2,9 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { HiEnvelope, HiHome, HiUser, HiViewColumns } from 'react-icons/hi2';
 
-import { NavBarData } from '@/constants/data/NavBarData';
+import { PAGES } from '@/constants';
 
 type Props = {
   lang: string;
@@ -11,6 +12,18 @@ type Props = {
 
 const Navbar = ({ lang }: Props) => {
   const pathname = usePathname();
+  const navBarMenu = [
+    { id: 1, name: 'Home', path: PAGES.HOME, icon: <HiHome /> },
+    { id: 2, name: 'About', path: PAGES.ABOUT, icon: <HiUser /> },
+    { id: 3, name: 'Work', path: PAGES.WORK, icon: <HiViewColumns /> },
+    {
+      id: 4,
+      name: 'Contact',
+      path: PAGES.CONTACT,
+      icon: <HiEnvelope />,
+    },
+  ];
+
   return (
     <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
       {/* inner */}
@@ -18,7 +31,7 @@ const Navbar = ({ lang }: Props) => {
         className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[60px] xl:h-max py-8 bg-white/10
       backdrop-blur-sm text-2xl xl:text-xl xl:rounded-full'
       >
-        {NavBarData.map((link) => {
+        {navBarMenu.map((link) => {
           const currentPath = pathname.replace(/^\/(en|ja)/, '') || '/';
           return (
             <Link
@@ -31,9 +44,7 @@ const Navbar = ({ lang }: Props) => {
               {/* tooltip */}
               <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
                 <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
-                  <div className='text-[12px] leading-none font-semibold capitalize'>
-                    {link.name}
-                  </div>
+                  <div className='text-[12px] leading-none font-semibold'>{link.name}</div>
                   {/* triangle */}
                   <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2' />
                 </div>
--- src/features/Work/_components/WorkSlider.tsx
+++ src/features/Work/_components/WorkSlider.tsx
@@ -1,66 +1,318 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaReact } from 'react-icons/fa';
+import {
+  SiJavascript,
+  SiTypescript,
+  SiNodedotjs,
+  SiThreedotjs,
+  SiExpress,
+  SiNextdotjs,
+  SiDocker,
+  SiAmazonaws,
+  SiMongodb,
+  SiSupabase,
+  SiPrisma,
+  SiTailwindcss,
+  SiMui,
+  SiBootstrap,
+  SiStyledcomponents,
+} from 'react-icons/si';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-import { workSlider } from '@/constants/data/workData';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
-const WorkSlider = () => (
-  <Swiper
-    spaceBetween={10}
-    pagination={{
-      clickable: true,
-    }}
-    modules={[Pagination]}
-    className='h-[280px] sm:h-[480px]'
-  >
-    {workSlider.slides.map((slide) => (
-      <SwiperSlide key={slide.id}>
-        <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
-          {slide.images.map((image) => (
-            <div
-              className='relative rounded-lg overflow-hidden flex items-center justify-center group'
-              key={image.id}
-            >
-              <div className='flex items-center justify-center relative overflow-hidden group'>
-                {/* image */}
-                <Image src={image.path} width={500} height={300} alt='project image' />
-                {/* overlay gradient */}
-                <Link href={image.url} rel='noopener noreferrer' target='_blank'>
-                  <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#236a78] to-[#0CC7AB] opacity-0 group-hover:opacity-80 transition-all duration-700' />
-                </Link>
-                {/* title */}
-                <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
-                  <div className='flex flex-col items-center gap-y-2 text-[13px] lg:text-[18px] tracking-[0.2em]'>
-                    <span className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-100'>
-                      {image.title}
-                    </span>
-                    <span className='flex gap-x-1 lg:gap-x-2 translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
-                      {image.teck.map((teck) => (
-                        <span key={teck.id}>{teck.icon}</span>
-                      ))}
-                    </span>
-                    <span className='absolute bottom-[-30px] lg:bottom-[-50px] translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-300'>
-                      {image.gitUrl && (
-                        <Link href={image.gitUrl} rel='noopener noreferrer' target='_blank'>
-                          <FaGithub />
-                        </Link>
-                      )}
-                    </span>
+const WorkSlider = () => {
+  const workSlider = {
+    slides: [
+      {
+        id: 1,
+        images: [
+          {
+            id: 1,
+            title: 'luluti',
+            path: '/images/works/luluti.png',
+            url: 'https://luluti.jp/?gad=1&gclid=Cj0KCQjwm66pBhDQARIsALIR2zAI69HFnuHbwVbjeeiV58TIy15ffxPqmmFToRYVj7JOBZnPnydQ1A0aAlScEALw_wcB',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              { id: 3, icon: <SiNextdotjs /> },
+              {
+                id: 4,
+                icon: <SiTailwindcss />,
+              },
+              { id: 5, icon: <SiDocker /> },
+            ],
+          },
+          {
+            id: 2,
+            title: 'mummar',
+            path: '/images/works/mummar.png',
+            url: 'https://mummar.com/',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              { id: 3, icon: <SiNextdotjs /> },
+              {
+                id: 4,
+                icon: <SiTailwindcss />,
+              },
+              { id: 5, icon: <SiDocker /> },
+            ],
+          },
+          {
+            id: 3,
+            title: 'Social Media',
+            path: '/images/works/Social-media.png',
+            url: 'https://next-sns-alpha.vercel.app/',
+            gitUrl: 'https://github.com/likuor/next-sns-client',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              { id: 3, icon: <SiNextdotjs /> },
+              {
+                id: 4,
+                icon: <SiTailwindcss />,
+              },
+              {
+                id: 5,
+                icon: <SiNodedotjs />,
+              },
+              {
+                id: 6,
+                icon: <SiExpress />,
+              },
+              {
+                id: 7,
+                icon: <SiPrisma />,
+              },
+              {
+                id: 8,
+                icon: <SiSupabase />,
+              },
+            ],
+          },
+          {
+            id: 4,
+            title: 'Supabase Blog',
+            path: '/images/works/Supabase-blog.png',
+            url: 'https://blog-supabase-five.vercel.app/',
+            gitUrl: 'https://github.com/likuor/blog-supabase',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              { id: 3, icon: <SiNextdotjs /> },
+              {
+                id: 4,
+                icon: <SiTailwindcss />,
+              },
+              {
+                id: 5,
+                icon: <SiPrisma />,
+              },
+              {
+                id: 6,
+                icon: <SiSupabase />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 2,
+        images: [
+          {
+            id: 1,
+            title: 'Portfolio-v2',
+            path: '/images/works/portfoliov2.png',
+            url: 'https://portfolio-v1-1-dev.vercel.app/work',
+            gitUrl: 'https://github.com/likuor/portfolio-v1.1',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              { id: 3, icon: <SiNextdotjs /> },
+              {
+                id: 4,
+                icon: <SiTailwindcss />,
+              },
+              {
+                id: 5,
+                icon: <SiThreedotjs />,
+              },
+            ],
+          },
+          {
+            id: 2,
+            title: 'Chillip',
+            path: '/images/works/Chillip.png',
+            url: 'https://main.d1q845p9ygn1yh.amplifyapp.com/login',
+            gitUrl: 'https://github.com/tenmusu007/matchingapp-frontend',
+            teck: [
+              { id: 1, icon: <SiJavascript /> },
+              { id: 2, icon: <FaReact /> },
+              {
+                id: 3,
+                icon: <SiMui />,
+              },
+              {
+                id: 4,
+                icon: <SiNodedotjs />,
+              },
+              {
+                id: 5,
+                icon: <SiExpress />,
+              },
+              {
+                id: 6,
+                icon: <SiMongodb />,
+              },
+              {
+                id: 7,
+                icon: <SiAmazonaws />,
+              },
+            ],
+          },
+          {
+            id: 3,
+            title: 'Mongolog',
+            path: '/images/works/blogapp.png',
+            url: 'https://blogapp-bykoki.onrender.com',
+            gitUrl: 'https://github.com/likuor/blogapp',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              {
+                id: 3,
+                icon: <SiBootstrap />,
+              },
+              {
+                id: 4,
+                icon: <SiNodedotjs />,
+              },
+              {
+                id: 5,
+                icon: <SiExpress />,
+              },
+              {
+                id: 6,
+                icon: <SiMongodb />,
+              },
+            ],
+          },
+          {
+            id: 4,
+            title: 'Portfolio-v1',
+            path: '/images/works/portfolio.png',
+            url: 'https://kokisakai.netlify.app/',
+            gitUrl: 'https://github.com/likuor/portfolio-ts',
+            teck: [
+              { id: 1, icon: <SiTypescript /> },
+              { id: 2, icon: <FaReact /> },
+              {
+                id: 3,
+                icon: <SiStyledcomponents />,
+              },
+              {
+                id: 4,
+                icon: <SiThreedotjs />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 3,
+        images: [
+          {
+            id: 1,
+            title: 'Katsuraba',
+            path: '/images/works/Katsuraba_kitchen.png',
+            url: 'https://katsuraba-kitchien.netlify.app/',
+            gitUrl: 'https://github.com/likuor/katsuraba',
+            teck: [
+              { id: 1, icon: <SiJavascript /> },
+              { id: 2, icon: <FaReact /> },
+              {
+                id: 3,
+                icon: <SiTailwindcss />,
+              },
+            ],
+          },
+          {
+            id: 2,
+            title: 'Movie App',
+            path: '/images/works/movie_app.png',
+            url: 'https://movieapp-bykoki.netlify.app/',
+            gitUrl: 'https://github.com/likuor/movieapp',
+            teck: [
+              { id: 1, icon: <SiJavascript /> },
+              { id: 2, icon: <FaReact /> },
+              {
+                id: 3,
+                icon: <SiTailwindcss />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className='h-[280px] sm:h-[480px]'
+    >
+      {workSlider.slides.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+            {slide.images.map((image) => (
+              <div
+                className='relative rounded-lg overflow-hidden flex items-center justify-center group'
+                key={image.id}
+              >
+                <div className='flex items-center justify-center relative overflow-hidden group'>
+                  {/* image */}
+                  <Image src={image.path} width={500} height={300} alt='project image' />
+                  {/* overlay gradient */}
+                  <Link href={image.url} rel='noopener noreferrer' target='_blank'>
+                    <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#236a78] to-[#0CC7AB] opacity-0 group-hover:opacity-80 transition-all duration-700' />
+                  </Link>
+                  {/* title */}
+                  <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                    <div className='flex flex-col items-center gap-y-2 text-[13px] lg:text-[18px] tracking-[0.2em]'>
+                      <span className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-100'>
+                        {image.title}
+                      </span>
+                      <span className='flex gap-x-1 lg:gap-x-2 translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+                        {image.teck.map((teck) => (
+                          <span key={teck.id}>{teck.icon}</span>
+                        ))}
+                      </span>
+                      <span className='absolute bottom-[-30px] lg:bottom-[-50px] translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-300'>
+                        {image.gitUrl && (
+                          <Link href={image.gitUrl} rel='noopener noreferrer' target='_blank'>
+                            <FaGithub />
+                          </Link>
+                        )}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </SwiperSlide>
-    ))}
-  </Swiper>
-);
+            ))}
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
 export default WorkSlider;
